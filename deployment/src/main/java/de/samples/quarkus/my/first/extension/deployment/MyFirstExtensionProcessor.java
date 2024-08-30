6e89e7b7c1b7fd4b3812a28cfbe2b9627d084f47
Ralf Ueberfuhr <ralf.ueberfuhr@yahoo.com>
2024-08-30 23:04:46 +0200

    Initial commit

---
+++ deployment/src/main/java/de/samples/quarkus/my/first/extension/deployment/MyFirstExtensionProcessor.java
@@ -0,0 +1,14 @@
+package de.samples.quarkus.my.first.extension.deployment;
+
+import io.quarkus.deployment.annotations.BuildStep;
+import io.quarkus.deployment.builditem.FeatureBuildItem;
+
+class MyFirstExtensionProcessor {
+
+    private static final String FEATURE = "my-first-extension";
+
+    @BuildStep
+    FeatureBuildItem feature() {
+        return new FeatureBuildItem(FEATURE);
+    }
+}
