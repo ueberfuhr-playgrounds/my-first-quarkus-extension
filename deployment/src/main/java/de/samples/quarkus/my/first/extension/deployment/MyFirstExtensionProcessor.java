69ee47f8b8b9acfd477ce75c4b8df6e2c9aab43b
Ralf Ueberfuhr <ralf.ueberfuhr@yahoo.com>
2024-08-30 23:07:43 +0200

    Reformat code

--- deployment/src/main/java/de/samples/quarkus/my/first/extension/deployment/MyFirstExtensionProcessor.java
+++ deployment/src/main/java/de/samples/quarkus/my/first/extension/deployment/MyFirstExtensionProcessor.java
@@ -5,10 +5,10 @@ import io.quarkus.deployment.builditem.FeatureBuildItem;
 
 class MyFirstExtensionProcessor {
 
-    private static final String FEATURE = "my-first-extension";
+  private static final String FEATURE = "my-first-extension";
 
-    @BuildStep
-    FeatureBuildItem feature() {
-        return new FeatureBuildItem(FEATURE);
-    }
+  @BuildStep
+  FeatureBuildItem feature() {
+    return new FeatureBuildItem(FEATURE);
+  }
 }
